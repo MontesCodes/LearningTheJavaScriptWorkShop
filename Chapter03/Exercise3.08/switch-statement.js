f9c4169ead232e2ebb303dfefe160a93d3c99756
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-11 20:34:01 -0400

    Updating Ch3 Ex3.08 9-11-2023

--- Chapter03/Exercise3.08/switch-statement.js
+++ Chapter03/Exercise3.08/switch-statement.js
@@ -1,15 +1,21 @@
-var keyNames = 'WASDwasd';
+var keyNames = 'WASDwasdIJKMijkm';
 var keyName = keyNames.charAt(Math.floor(Math.random() * keyNames.length));
-console.log('keyName:', keyName);
-
 console.log('keyName:', keyName);
 switch (keyName.toLowerCase()) {
   case 'a':
+  case 'j':
     console.log('move left');
+    break;
   case 'd':
+  case 'k':
     console.log('move right');
+    break;
   case 'w':
+  case 'i':
     console.log('move up');
+    break;
   case 's':
+  case 'm':
     console.log('move down');
+    break;
 }
