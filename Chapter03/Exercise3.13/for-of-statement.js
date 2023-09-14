c550349c59ace0bf88dbbdd612492718e3e63b9e
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-14 06:36:46 -0400

    updating Ch3 Exercise 3.13 9-14-2023

--- Chapter03/Exercise3.13/for-of-statement.js
+++ Chapter03/Exercise3.13/for-of-statement.js
@@ -1,29 +1,37 @@
-var stopWatch = {
-  elapsedTime: 0,
-  resultsHistory: [],
-  isTiming: true,
-  isPaused: true,
-  start: function () {
-    console.log('start');
-  },
-  pause: function () {
-    console.log('pause');
-  },
-  resume: function () {
-    console.log('resume');
-  },
-  stop: function () {
-    console.log('stop');
-  },
-};
-// for (let name of stopWatch) {
-//   console.log(name, ':', stopWatch[name]);
-// }
+// var stopWatch = {
+//   elapsedTime: 0,
+//   resultsHistory: [],
+//   isTiming: true,
+//   isPaused: true,
+//   start: function () {
+//     console.log('start');
+//   },
+//   pause: function () {
+//     console.log('pause');
+//   },
+//   resume: function () {
+//     console.log('resume');
+//   },
+//   stop: function () {
+//     console.log('stop');
+//   },
+// };
 
-let anyString = 'abcxyz123';
-// for (const value of anyString) {
-//   console.log(value);
+// // for (let name of stopWatch) {
+// //   console.log(name, ':', stopWatch[name]);
+// // }
+
+// let anyString = 'abcxyz123';
+
+// // for (const value of anyString) {
+// //   console.log(value);
+// // }
+
+// for (var i = 0; i < anyString.length; i++) {
+//   console.log(anyString.charAt(i));
 // }
-for (var i = 0; i < anyString.length; i++) {
-  console.log(anyString.charAt(i));
+let bowlingScores = [150, 160, 144, 190, 210, 185];
+
+for (const value of bowlingScores) {
+  console.log(value);
 }
