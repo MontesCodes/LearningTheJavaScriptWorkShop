3e8aadfab9cdcf8b8ef663b21904582f1fbd8607
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-14 06:41:59 -0400

    finishing Ch3 Exercise 3.13 9-14-2023

--- Chapter03/Exercise3.13/for-of-statement.js
+++ Chapter03/Exercise3.13/for-of-statement.js
@@ -32,6 +32,10 @@
 // }
 let bowlingScores = [150, 160, 144, 190, 210, 185];
 
-for (const value of bowlingScores) {
-  console.log(value);
+// for (const value of bowlingScores) {
+//   console.log(value);
+// }
+
+for (var i = 0; i < bowlingScores.length; i++) {
+  console.log(bowlingScores[i]);
 }
