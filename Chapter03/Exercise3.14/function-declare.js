ec0b84ef9e901a06315c1224f68c597533bb9139
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-14 20:31:39 -0400

    Updating Ch3 Exercise 3.14 9-14-2023

---
+++ Chapter03/Exercise3.14/function-declare.js
@@ -0,0 +1,11 @@
+var test123 = getDigitsOfNumber(123);
+console.log('test123:', test123);
+
+function getDigitsOfNumber(num) {
+  var digits = [];
+  console.log('num:', num);
+  return digits;
+}
+var test456 = getDigitsOfNumber(456);
+console.log('test456:', test456);
+// console.log('getDigitsOfNumber:', getDigitsOfNumber);
