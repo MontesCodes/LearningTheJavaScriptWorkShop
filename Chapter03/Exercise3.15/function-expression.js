7af4d05208fb9760bf24b671d78ce922a0bd5b5f
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-14 21:17:00 -0400

    Updating Ch3 Exercise 3.15 9-14-2023

---
+++ Chapter03/Exercise3.15/function-expression.js
@@ -0,0 +1,8 @@
+var getRandomStringCharacter = function (source) {
+  var returnCharacter = '';
+  console.log('source:', source);
+  return returnCharacter;
+};
+// console.log('getRandomStringCharacter', getRandomStringCharacter);
+getRandomStringCharacter();
+getRandomStringCharacter('AEIOU');
