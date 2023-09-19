da858d18c0d8a6f3e0f8db1bca71fe962910dfd3
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-18 21:15:19 -0400

    Updating Ch3 Ex.3.17 number-guess.js 9-18-2023

--- Chapter03/Exercise3.17/number-guess.js
+++ Chapter03/Exercise3.17/number-guess.js
@@ -8,15 +8,22 @@ testButtonEle.addEventListener('click', testMatch);
 
 function testMatch(e) {
   //   console.log('Clicked!');
+  matchedMsgEle.style.display = 'none';
+  noMatchMsgEle.style.display = 'none';
+  resultsMsgEle.style.display = 'none';
   let numberGuessed = parseInt(guessInputEle.value);
   if (!isNaN(numberGuessed) && numberGuessed > 0 && numberGuessed <= 10) {
+    resultsMsgEle.style.display = 'block';
     let numberToGuess = Math.floor(Math.random() * 10 + 1);
     if (numberGuessed == numberToGuess) {
-      console.log('MATCHED!');
+      matchedMsgEle.style.display = 'inline';
+      //  console.log('MATCHED!');
     } else {
-      console.log('NOT MATCHED!');
+      noMatchMsgEle.style.display = 'inline';
+      //  console.log('NOT MATCHED!');
     }
-    console.log('Number gessed:', numberGuessed);
-    console.log('Number to match:', numberToGuess);
+    numberToGuessEle.innerText = numberToGuess;
+    //     console.log('Number guessed:', numberGuessed);
+    //     console.log('Number to match:', numberToGuess);
   }
 }
