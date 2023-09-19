40829dc0ee397799caa0ae404644c7a1a6fe2f9d
MontesCode13Surface5 <MontiesWebDev@Gmail.com>
2023-09-18 20:57:10 -0400

    Updating Ch3 Exercise 3.17 9-18-2023

--- Chapter03/Exercise3.17/number-guess.js
+++ Chapter03/Exercise3.17/number-guess.js
@@ -1,3 +1,8 @@
+let resultsMsgEle = document.getElementById('results-msg');
+let matchedMsgEle = document.getElementById('match-msg');
+let noMatchMsgEle = document.getElementById('no-match-msg');
+let numberToGuessEle = document.getElementById('number-to-guess');
+let guessInputEle = document.getElementById('number-guessed');
 let testButtonEle = document.getElementById('test-button');
 testButtonEle.addEventListener('click', testMatch);
 
