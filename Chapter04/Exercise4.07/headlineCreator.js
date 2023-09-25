47f695fc796b575cc8f214abf4148792f0248c42
MontesCodesLT-1557 <MontiesWebDev@Gmail.com>
2023-09-25 07:33:28 -0400

    finished Ch 4 Exercise 4.07 9/25/2023

--- Chapter04/Exercise4.07/headlineCreator.js
+++ Chapter04/Exercise4.07/headlineCreator.js
@@ -1,13 +1,35 @@
 // headlineCreator.js
-const headlineCreator = (() => {
-  function createHeadline(text, parentElementSelector = '#root') {}
-  const parentElement = document.querySelector(parentElementSelector);
-  const headlineToInsert = document.createElement('h1');
-  headlineToInsert.innerHTML = text;
-  parentElement.appendChild(headlineToInsert);
-  {
+// code below is from following book instructions
+// const headlineCreator = (() => {
+//   function createHeadline(text, parentElementSelector = '#root') {}
+//   const parentElement = document.querySelector(parentElementSelector);
+//   const headlineToInsert = document.createElement('h1');
+//   headlineToInsert.innerHTML = text;
+//   parentElement.appendChild(headlineToInsert);
+//   {
+//   }
+//   return {
+//     createHeadline: createHeadline,
+//   };
+// })();
+// code below type of from GitHub repository of book due to book instruction no clear to what needs to be typed
+window.headlineCreator = (function () {
+  function createHeadline(text, parentElementSelector = '#root') {
+    const parentElement = document.querySelector(parentElementSelector);
+    if (!test) {
+      throw new Error('You Forgot To Pass The "text" Parameter');
+    }
+    if (!parentElement) {
+      throw new Error(
+        `There was no node found for the Selector: "${parentElementSelector}"`,
+      );
+    }
+    const headlineToInsert = document.createElement('h1');
+    headlineToInsert.innerText = text;
+    parentElement.appendChild(headlineToInsert);
+    return headlineToInsert;
   }
   return {
-    createHeadline: createHeadline,
+    createHeadline,
   };
 })();
