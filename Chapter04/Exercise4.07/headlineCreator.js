5828e8b955f122274d3a62abad5878558b724742
MontesCodes <MontiesWebDev@Gmail.com>
2023-09-24 21:59:25 -0400

    code not matching up with text in box Ch 4 Exercise 4.07 9/24/2023

--- Chapter04/Exercise4.07/headlineCreator.js
+++ Chapter04/Exercise4.07/headlineCreator.js
@@ -1,2 +1,13 @@
 // headlineCreator.js
-const headlineCreator = (function () {})();
+const headlineCreator = (() => {
+  function createHeadline(text, parentElementSelector = '#root') {}
+  const parentElement = document.querySelector(parentElementSelector);
+  const headlineToInsert = document.createElement('h1');
+  headlineToInsert.innerHTML = text;
+  parentElement.appendChild(headlineToInsert);
+  {
+  }
+  return {
+    createHeadline: createHeadline,
+  };
+})();
