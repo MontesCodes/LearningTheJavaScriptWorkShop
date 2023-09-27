4fad72199f4ed9294a4a1be3a5b258a7411d99ca
MontesCodes <MontiesWebDev@Gmail.com>
2023-09-26 20:56:56 -0400

    updating Ch 5 Exercise 5.02 9/26/2023

--- Chapter05/Exercise5.02/index.js
+++ Chapter05/Exercise5.02/index.js
@@ -1,12 +1,11 @@
-var printType = function(val) {
+var printType = function (val) {
   if (val === null) {
-    console.log("Value is null");
+    console.log('Value really is null');
     return;
   }
-  console.log("Value is", typeof val);
-}
-
-printType(12);  // ->  Value is number
-printType("I am a string");  // ->  Value is string
-printType({});  // ->  Value is object
-printType(null);  // ->  Value is null
+  console.log('Value is', typeof val);
+};
+printType(12);
+printType('I am a string');
+printType({});
+printType(null);
