6b94058ec38cf301752c749e19b30315740c973e
MontesCodes <MontiesWebDev@Gmail.com>
2023-09-28 20:12:25 -0400

    Updating Ch 5 Exercise 5.04 9/28/2023

--- Chapter05/Exercise5.04/index.js
+++ Chapter05/Exercise5.04/index.js
@@ -1,12 +1,9 @@
+// Free Home Delivery Eligibility Validation
 function isEligible(distance, membershipstatus) {
-  if (distance < 1 || membershipstatus === "active" && distance > 10 || membershipstatus === "inactive" && distance > 5 ) {
-    return false;
+  if (
+    distance < 1 ||
+    (membershipstatus === 'active' && distance > 10) ||
+    (membershipstatus === 'inactive' && distance > 5)
+  ) {
   }
-  return true;
 }
-console.log( isEligible(.5, "active") );
-// =>   false
-console.log( isEligible(7, "inactive") );
-// =>   false
-console.log( isEligible(7, "active") );
-// =>    true
