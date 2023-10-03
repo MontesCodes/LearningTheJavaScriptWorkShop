fcbf88a2a303fdfb5d8d6bb1e50ce24f685e685f
MontesCodesLT-1557 <MontiesWebDev@Gmail.com>
2023-10-03 07:13:24 -0400

    finished Ch 5 Exercise5.05 10/3/2023

--- Chapter05/Exercise5.05/index.js
+++ Chapter05/Exercise5.05/index.js
@@ -1,4 +1,13 @@
 function formatPrice(value, currency) {
   value = Number(value);
   currency = currency || '$';
+  if (Number.isNaN(value) || typeof currency != 'string') {
+    return null;
+  }
+  return currency + value.toFixed(2);
 }
+console.log(formatPrice(1.99, 32));
+console.log(formatPrice(5, '^'));
+console.log(formatPrice(9.9));
+console.log(formatPrice('Ted'));
+console.log(formatPrice('Ted', '&'));
