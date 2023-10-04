ec4a121068691feb645648df3cf771c63f7ae28b
MontesCodes <MontiesWebDev@Gmail.com>
2023-10-03 21:53:01 -0400

    finished Ch 5 Exercise 5.06 10/3/2023

--- Chapter05/Exercise5.06/index.js
+++ Chapter05/Exercise5.06/index.js
@@ -1,21 +1,22 @@
 function sendEmail(name, age, comments) {
   var age = Number(age);
-  if (Number.isNaN(age)
-  || typeof name != "string"
-  || typeof comments != "string") {
+  if (
+    Number.isNaN(age) ||
+    typeof name != 'string' ||
+    typeof comments != 'string'
+  ) {
     return null;
   }
-  var body = `A user has posted a comment from the website:
+  var body = `A user Has Posted A Comment From The Website:
   name: ${name}
   age: ${age}
-  status: ${(age < 18) ? `${name} is not a valid user` : `${name} is a valid user`}
+  status: ${
+    age < 18
+      ? `${name} is not a valid user`
+      : `${name} is a valid 
+  user`
+  }
   comments: ${comments}`;
   return body;
 }
-
-sendEmail("Jane", 27, "Your website is fantastic!");
-// =>  "A user has posted a comment from the website:
-// =>  name: Jane
-// =>  age: 27
-// =>  status: Jane is an adult
-// =>  comments Your website is fantastic!"
+sendEmail('Kelly', 39, 'Your WebSite is Fantastic!');
