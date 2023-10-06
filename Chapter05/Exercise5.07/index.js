1ace36eb542c521e51644ac0f3de6b791cf7d06c
MontesCodes <MontiesWebDev@Gmail.com>
2023-10-05 21:51:35 -0400

    Finished Ch 5 Exercise 5.07 10/5/2023

--- Chapter05/Exercise5.07/index.js
+++ Chapter05/Exercise5.07/index.js
@@ -10,18 +10,27 @@ function reverse(str) {
       chr = chr.toUpperCase();
       isStart = false;
     } else {
-      chr = chr.toLowerCase;
+      chr = chr.toLowerCase();
     }
     if (chr == '.') {
       isStart = true;
       if (index == str.length - 1) {
         index--;
         continue;
+      } else {
+        result = result.substr(0, result.length - 1) + '. ';
+        index--;
+        continue;
       }
-    } else {
-      result = result.substr(0, result.length - 1) + '. ';
-      index--;
-      continue;
     }
+    result = result + chr;
+    index--;
   }
+  return result + '.';
 }
+
+console.log(
+  reverse(
+    'This is the first Sentence of the day. Following with the second sentence of the day.',
+  ),
+);
