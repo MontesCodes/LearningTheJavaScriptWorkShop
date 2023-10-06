4139815046599765a49fb64e7e8c4b18823062c1
MontesCodes <MontiesWebDev@Gmail.com>
2023-10-05 21:10:30 -0400

    Updating Ch 5 Exercise 5.07 10/5/2023

--- Chapter05/Exercise5.07/in.js
+++ Chapter05/Exercise5.07/in.js
@@ -1,1 +1,10 @@
-function reverse(str) {}
+function reverse(str) {
+  str = String(str).trim();
+  var result = '',
+    index = str.length - 1,
+    chr,
+    isStart = true;
+  while (index >= 0) {
+    chr = str[index];
+  }
+}
